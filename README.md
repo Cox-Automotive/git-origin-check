72cc55fe5dc7f1c033406d8352b2df3d39e480d1
Brian Antonelli <brian.antonelli@gmail.com>
2020-11-19 15:20:26 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # git-origin-check
-Validates Git origin is set to correct host.
+
+Validates Git origin is set to correct host. When used in combination with Husky as a `pre-push` hook it enables users to ensure code is not inadvertently pushed to the wrong Github instance.
+
+Example Usage:
+
+```
+npm install git-origin-check
+git-origin check github.com
+```
---
+++ bin/run.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const originCheck = require('../lib/originCheck.js');
+
+const args = process.argv.slice(2);
+if(args.length !== 1) {
+  console.log('Please supply origin hostname.');
+  process.exit(1);
+}
+
+(async () => {
+  try{
+    await originCheck.check(args[0]);
+  } catch(e){
+    console.error(e);
+    process.exit(1);
+  }
+
+  process.exit(0);
+})();
