75dbf45bead82d60956de061fb8207829e3b8347
Brian Antonelli <brian.antonelli@gmail.com>
2020-11-20 10:31:12 -0500

    Fix error message, added more tests

--- test/originCheck.js
+++ test/originCheck.js
@@ -1,5 +1,7 @@
-const originCheck = require('../lib/originCheck.js');
 const assert = require('chai').assert;
+const rewire = require('rewire');
+const sinon = require('sinon');
+const originCheck = rewire('../lib/originCheck.js');
 
 describe('git-origin-check', async () => {
   it('should pass with default repo of github.com', async () => {
@@ -12,6 +14,22 @@ describe('git-origin-check', async () => {
       assert(false, 'github.co shouldnt pass');
     } catch(e){
       assert(true, 'github.co should fail');
+    }
+  });
+
+  it('should fail when host in .git/config doesnt match', async () => {
+    const checkObj = { getRemoteOriginUrl: originCheck.__get__('getRemoteOriginUrl') };
+    const stubGetOrigin = sinon
+      .stub(checkObj, 'getRemoteOriginUrl').callsFake(async () => {
+        return 'https://github.net/Cox-Automotive/git-origin-check.git';
+      });
+    originCheck.__set__('getRemoteOriginUrl', stubGetOrigin)
+
+    try {
+      await originCheck.check('github.com');
+      assert(false, '.git/config shouldnt pass when its set to github.net');
+    } catch (e) {
+      assert(true, '.git/config should fail when its set to github.net');
     }
   });
 
